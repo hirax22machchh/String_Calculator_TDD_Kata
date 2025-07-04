7d986a4a572721066905b3b57cc725a50eb6fa9c
hirax22machchh <hirax.machchh@gmail.com>
2025-07-05 00:29:54 +0530

    [ 2 ] step-1 completed with (empty string, single number, two numbers, more than two numbers) Positive Test cases.

--- src/test/java/com/incubyte/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/incubyte/stringcalculator/StringCalculatorTest.java
@@ -4,10 +4,37 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculatorTest {
+
+    //for empty string - returns 0
+    @Test
+    void addEmptyStringReutrnsZero(){
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("");
+        assertEquals(0,result);
+    }
+
+    //for single number - returns number itself
+    @Test
+    void addSingleNumberReturnsItself(){
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("22");
+        assertEquals(22,result);
+    }
+
+    //for two numbers - returns sum of two numbers
+    @Test
+    void addTwoCommaSeparatedNumbersReturnsTheirSum() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("1,5");
+        assertEquals(6, result);
+    }
+
+
+    //for more than two numbers - returns sum of them
     @Test
     void addCommaSeparatedNumbersReturnsSum(){
         StringCalculator calculator = new StringCalculator();
-        int result = calculator.add("1,2,3");
-        assertEquals(6,result);
+        int result = calculator.add("2,4,6");
+        assertEquals(12,result);
      }
 }
